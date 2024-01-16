56fda0056cb4e58699761ed22b358608578feff7
inlibra-cpa <inlibra@gmail.com>
2024-01-16 01:40:46 -0500

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+
+Event.create(title: 'Meeting with team', date: '2024-01-20 10:00:00', description: 'Weekly team meeting.')
+Event.create(title: 'Doctor Appointment', date: '2024-01-22 15:00:00', description: 'Annual check-up.')
